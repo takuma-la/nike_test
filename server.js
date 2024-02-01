08cb260ba10335e141c79b700d56861b85ad0665
takumaienaga@gmail.com <takuma#55>
2024-02-01 15:32:54 -0800

    change server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import path from 'path';
 const app = express();
 const port = 3000; // 使用するポート番号
+
+// 静的ファイルを配信するためのディレクトリを指定
+app.use(express.static('public'));
 
 // ルートパスにアクセスがあった場合の処理
 app.get('/', (req, res) => {
