2eb6bcdeec63c83a7241907e31535a5ae5aeba53
takumaienaga@gmail.com <takuma#55>
2024-02-05 07:28:24 -0800

    Edit: main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // ルートコンポーネント
-import './index.css'; // アプリケーション全体のスタイルシート
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-// 必要に応じてその他のライブラリをimport
-// import { BrowserRouter as Router } from 'react-router-dom'; // ルーティング用のライブラリ
-// import { Provider } from 'react-redux'; // Reduxを使用する場合
-// import store from './store'; // Reduxのストア
-
-// アプリケーションのレンダリング
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Router>
-      <Provider store={store}> */}
-        <App />
-      {/* </Provider>
-    </Router> */}
+    <App />
   </React.StrictMode>,
-);
+)
