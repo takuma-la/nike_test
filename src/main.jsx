b663f5030ad5ac49312f894fe191a4ef3f34da3d
takumaienaga@gmail.com <takuma#55>
2024-02-05 07:26:36 -0800

    add:babel/core for jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // ルートコンポーネント
+import './index.css'; // アプリケーション全体のスタイルシート
 
+// 必要に応じてその他のライブラリをimport
+// import { BrowserRouter as Router } from 'react-router-dom'; // ルーティング用のライブラリ
+// import { Provider } from 'react-redux'; // Reduxを使用する場合
+// import store from './store'; // Reduxのストア
+
+// アプリケーションのレンダリング
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <Router>
+      <Provider store={store}> */}
+        <App />
+      {/* </Provider>
+    </Router> */}
   </React.StrictMode>,
-)
+);
