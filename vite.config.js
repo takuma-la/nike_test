21a76227102cabc4ade78abb6ea933ff2c27c474
takumaienaga@gmail.com <takuma#55>
2024-02-07 19:57:00 -0800

    checking project_branch

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: '/nike_test/',
+  base: '/nike_project/nike_test/',
   plugins: [react()],
 })
